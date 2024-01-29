9473ffc27c542195cb9801d16f476122304da788
junghyunlee0922 <self21c123@naver.com>
2024-01-29 22:38:28 +0900

    [Test] 커밋 test

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import App from './app/App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+// 안녕 나는 이정현이야
+// 만나서 반가워
 root.render(
   <StrictMode>
     <App />
